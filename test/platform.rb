8af36d88e199c8c50aaa2027e0192e34824ddc17
Keith Gable <ziggy@ignition-project.com>
2010-03-15 21:54:27 -0500

    Explicitly identify the module for cleaner code and because things would raise name errors

--- test/platform.rb
+++ test/platform.rb
@@ -1,14 +1,18 @@
 require "test/unit"
 
 # Does a test against MacroDeck::Platform
-class MacroDeck::Tests::Platform < Test::Unit::TestCase
-	# Starts the platform with known values.
-	def setup
-		MacroDeck::Platform.start!("macrodeck-test")
-	end
+module MacroDeck
+	module Tests
+		class Platform < Test::Unit::TestCase
+			# Starts the platform with known values.
+			def setup
+				MacroDeck::Platform.start!("macrodeck-test")
+			end
 
-	# Tests the database_name is set by MacroDeck::Platform
-	def test_database_name
-		assert_equal MacroDeck::Platform.database_name, "macrodeck-test"
+			# Tests the database_name is set by MacroDeck::Platform
+			def test_database_name
+				assert_equal MacroDeck::Platform.database_name, "macrodeck-test"
+			end
+		end
 	end
 end
