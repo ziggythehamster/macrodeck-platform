281566d129a8869c2a5b5066092dff4700b5b90a
Keith Gable <ziggy@ignition-project.com>
2010-03-15 21:53:47 -0500

    * Explicitly identify the module for cleaner code and because things would raise name errors * Set up the model's database connection by using Kernel.eval

--- lib/platform.rb
+++ lib/platform.rb
@@ -1,19 +1,25 @@
-require "model"
-require "data_object_definition"
+module MacroDeck
+	# This class starts up the platform, pulls data object definitions, etc.
+	#
+	# Usage:
+	# 
+	#     MacroDeck::Platform.start!(database_name)
+	#
+	# Calling it more than once will not start the platform more than once.
+	class Platform
+		class << self
+			attr_reader :database_name
 
-# This class starts up the platform, pulls data object definitions, etc.
-#
-# Usage:
-# 
-#     MacroDeck::Platform.start!(database_name)
-#
-# Calling it more than once will not start the platform more than once.
-class MacroDeck::Platform
-	class << self
-		attr_reader :database_name
+			def start!(database_name)
+				@database_name = database_name
 
-		def start!(database_name)
-			@database_name = database_name
+				# Add the use_database to the model
+				Kernel.eval "module MacroDeck
+					class Model < CouchRest::ExtendedDocument
+						use_database CouchRest.new.database!(\"#{@database_name}\")
+					end
+				end"
+			end
 		end
 	end
 end
