eee19082bd08a4737656940ff2828f58d08b26c1
Keith Gable <ziggy@ignition-project.com>
2010-03-15 21:52:46 -0500

    Explicitly identify the module for cleaner code and because things would raise name errors

--- lib/model.rb
+++ lib/model.rb
@@ -1,10 +1,13 @@
+require "rubygems"
 require "validatable"
+require "couchrest"
+require "platform"
 
-# MacroDeck::Model is the base class for models provided by the platform.
-# Objects that are defined in the platform will inherit from DataObject.
-# This class inherits from CouchRest::ExtendedDocument.
-class MacroDeck::Model < CouchRest::ExtendedDocument
-	include Validatable
-
-	use_database CouchRest.new.database!(MacroDeck::Platform.database_name)
+module MacroDeck
+	# MacroDeck::Model is the base class for models provided by the platform.
+	# Objects that are defined in the platform will inherit from DataObject.
+	# This class inherits from CouchRest::ExtendedDocument.
+	class Model < CouchRest::ExtendedDocument
+		include Validatable
+	end
 end
