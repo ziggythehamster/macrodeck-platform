720e19bc1423978e3366e92b5578439cb2fb99a9
Keith Gable <ziggy@ignition-project.com>
2010-03-21 19:12:21 -0500

    Add code to DataObjectDefinition to define the dynamic class

--- lib/data_object_definition.rb
+++ lib/data_object_definition.rb
@@ -6,6 +6,9 @@ class DataObjectDefinition < MacroDeck::Model
 	property :fields
 	property :validations
 
+	# Attributes
+	attr_reader :defined # Is this object defined already?
+
 	# Validations that happen on this class.
 	validates_presence_of :object_type
 	validates_presence_of :fields
@@ -13,6 +16,34 @@ class DataObjectDefinition < MacroDeck::Model
 	validates_true_for :object_type, :logic => lambda { object_type.is_a?(String) }
 	validates_true_for :fields, :logic => :validate_fields, :message => "is not valid"
 	validates_true_for :validations, :logic => :validate_validations, :message => "is not valid"
+
+	# Executes the code necessary to define this object
+	def define!
+		properties = ""
+		class_body = ""
+
+		# Populate properties.
+		if self.valid?
+			# Iterate all of the fields and define them.
+			self.fields.each do |field|
+				symbol = field[0].to_sym.inspect
+				klass = eval(field[1].split(" ")[0]) # NB: This could potentially be a very unsafe operation...
+				properties << "property #{symbol}\n"
+				properties << "validates_true_for #{symbol}, :logic => lambda { #{field[0]}.is_a?(#{klass}) }\n"
+				properties << "validates_presence_of #{symbol}\n" if field[2] == true
+			end
+
+			# TODO: Iterate the validations and define them.
+
+			# Define the class.
+			klass = self.object_type.split(" ")[0]
+			class_body =
+				"class #{klass} < MacroDeck::Model
+					#{properties}
+				end"
+			puts class_body
+		end
+	end
 
 	private
 		# Returns true if the fields array is at least visibly valid.
