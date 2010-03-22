d90f92591fd145b6d66f23dc15e54796a924f679
Keith Gable <ziggy@ignition-project.com>
2010-03-21 19:50:52 -0500

    Add DataObjectDefinition to test suite

--- test/test_suite.rb
+++ test/test_suite.rb
@@ -1,11 +1,13 @@
 require 'test/unit/testsuite'
 require 'test/platform'
+require 'test/data_object_definition'
 
 module MacroDeck
 	class TestSuite
 		def self.suite
-			suite  = Test::Unit::TestSuite.new
+			suite  = Test::Unit::TestSuite.new("MacroDeck Platform Tests")
 			suite << MacroDeck::Tests::Platform.suite
+			suite << MacroDeck::Tests::DataObjectDefinition.suite
 
 			return suite
 		end
