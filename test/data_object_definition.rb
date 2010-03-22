cc7639744d2d68e9017800cbff56d20e09a003ca
Keith Gable <ziggy@ignition-project.com>
2010-03-21 19:32:33 -0500

    Start testing DataObjectDefinition

---
+++ test/data_object_definition.rb
@@ -0,0 +1,23 @@
+require "test/unit"
+
+# Does a test against DataObjectDefinition
+module MacroDeck
+	module Tests
+		class DataObjectDefinition < Test::Unit::TestCase
+			# Creates a test object and verifies it exists.
+			def test_001_create_test_definition
+				object = {
+					"object_type" => "MacroDeckTestObject",
+					"fields" => [
+						["not_required_str", "String", false],
+						["required_str", "String", true]
+					],
+					"validations" => []
+				}
+				test_object = DataObjectDefinition.new(object)
+				assert test_object.valid?
+				assert test_object.save
+			end
+		end
+	end
+end
