a53431e6923382d43ac0c8dbcac52ad0314b3e87
Keith Gable <ziggy@ignition-project.com>
2010-04-25 17:28:23 -0500

    Reverse the fields in the properties test

--- test/data_object_definition.rb
+++ test/data_object_definition.rb
@@ -67,12 +67,12 @@ module MacroDeck
 
 			# Tests that the object's properties gets populated.
 			def test_004_defined_object_properties
-				assert_equal ::MacroDeckTestObject.properties[0].name, "not_required_str"
-				assert_equal ::MacroDeckTestObject.properties[0].type, "String"
-				assert_equal ::MacroDeckTestObject.properties[1].name, "required_str"
-				assert_equal ::MacroDeckTestObject.properties[1].type, "String"
-				assert_equal ::MacroDeckTestObject.properties[2].name, "validated_str"
-				assert_equal ::MacroDeckTestObject.properties[2].type, "String"
+				assert_equal "not_required_str",	::MacroDeckTestObject.properties[0].name
+				assert_equal "String",			::MacroDeckTestObject.properties[0].type
+				assert_equal "required_str",		::MacroDeckTestObject.properties[1].name
+				assert_equal "String",			::MacroDeckTestObject.properties[1].type
+				assert_equal "validated_str",		::MacroDeckTestObject.properties[2].name
+				assert_equal "String",			::MacroDeckTestObject.properties[2].type
 			end
 
 			# Tests that the object's validations get populated.
