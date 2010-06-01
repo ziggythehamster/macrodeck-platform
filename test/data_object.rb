0f6aa59b90ab46f69a46bdde7833b1f41ef0b453
Keith Gable <ziggy@ignition-project.com>
2010-05-31 22:08:26 -0500

    Test find by human ID. Also update the rest of the tests to use unique keys. So the tests can't get mixed up.

--- test/data_object.rb
+++ test/data_object.rb
@@ -32,18 +32,23 @@ module MacroDeck
 				}
 				object2 = object.dup
 				object2["tags"] = ["test"]
-				object2["created_by"] = "user/ZiggyTheHamster"
+				object2["created_by"] = "user/testCreatedBy"
+				object2["updated_by"] = "user/testCreatedBy"
+				object2["owned_by"] = "user/testCreatedBy"
 				object2["title"] = "Test Alternate Created By"
 				object2["human_id"] = "alternate-created-by"
 				object3 = object2.dup
 				object3["title"] = "Test Alternate Updated By"
-				object3["updated_by"] = "user/csakon"
+				object3["created_by"] = "user/testUpdatedBy"
+				object3["updated_by"] = "user/testUpdatedBy"
+				object3["owned_by"] = "user/testUpdatedBy"
 				object3["human_id"] = "alternate-updated-by"
 				object4 = object3.dup
 				object4["title"] = "Test Alternate Owned By"
-				object4["owned_by"] = "user/yourmom"
+				object4["created_by"] = "user/testOwnedBy"
+				object4["updated_by"] = "user/testOwnedBy"
+				object4["owned_by"] = "user/testOwnedBy"
 				object4["human_id"] = "alternate-owned-by"
-
 
 				test_object = ::DataObject.new(object)
 				assert test_object.valid?
@@ -64,6 +69,7 @@ module MacroDeck
 			
 			# Tests that the previously created record can be retrieved.
 			def test_002_get_test_data_object_items
+				# Test find by title.
 				test_object = ::DataObject.view("by_title", :key => "Test Data Object Item", :include_docs => true)[0]
 				assert test_object.valid?
 				assert_equal [], test_object.path
@@ -75,39 +81,53 @@ module MacroDeck
 				assert_equal "A fake test item to test with.", test_object.description
 				assert_equal "test-data-object-item", test_object.human_id
 
-				test_object2 = ::DataObject.view("by_created_by", :key => "user/ZiggyTheHamster", :include_docs => true)[0]
+				# Test find by created by
+				test_object2 = ::DataObject.view("by_created_by", :key => "user/testCreatedBy", :include_docs => true)[0]
 				assert test_object2.valid?
 				assert_equal [], test_object2.path
 				assert_equal "Test Alternate Created By", test_object2.title
 				assert_equal ["test"], test_object2.tags
-				assert_equal "user/ZiggyTheHamster", test_object2.created_by
-				assert_equal "user/System", test_object2.updated_by
-				assert_equal "user/System", test_object2.owned_by
+				assert_equal "user/testCreatedBy", test_object2.created_by
+				assert_equal "user/testCreatedBy", test_object2.updated_by
+				assert_equal "user/testCreatedBy", test_object2.owned_by
 				assert_equal "A fake test item to test with.", test_object2.description
 				assert_equal "alternate-created-by", test_object2.human_id
 
-
-				test_object3 = ::DataObject.view("by_updated_by", :key => "user/csakon", :include_docs => true)[0]
+				# Test find by updated by
+				test_object3 = ::DataObject.view("by_updated_by", :key => "user/testUpdatedBy", :include_docs => true)[0]
 				assert test_object3.valid?
 				assert_equal [], test_object3.path
 				assert_equal "Test Alternate Updated By", test_object3.title
 				assert_equal ["test"], test_object3.tags
-				assert_equal "user/ZiggyTheHamster", test_object3.created_by
-				assert_equal "user/csakon", test_object3.updated_by
-				assert_equal "user/System", test_object3.owned_by
+				assert_equal "user/testUpdatedBy", test_object3.created_by
+				assert_equal "user/testUpdatedBy", test_object3.updated_by
+				assert_equal "user/testUpdatedBy", test_object3.owned_by
 				assert_equal "A fake test item to test with.", test_object3.description
 				assert_equal "alternate-updated-by", test_object3.human_id
 
-				test_object4 = ::DataObject.view("by_owned_by", :key => "user/yourmom", :include_docs => true)[0]
+				# Test find by owned by
+				test_object4 = ::DataObject.view("by_owned_by", :key => "user/testOwnedBy", :include_docs => true)[0]
 				assert test_object4.valid?
 				assert_equal [], test_object4.path
 				assert_equal "Test Alternate Owned By", test_object4.title
 				assert_equal ["test"], test_object4.tags
-				assert_equal "user/ZiggyTheHamster", test_object4.created_by
-				assert_equal "user/csakon", test_object4.updated_by
-				assert_equal "user/yourmom", test_object4.owned_by
+				assert_equal "user/testOwnedBy", test_object4.created_by
+				assert_equal "user/testOwnedBy", test_object4.updated_by
+				assert_equal "user/testOwnedBy", test_object4.owned_by
 				assert_equal "A fake test item to test with.", test_object4.description
 				assert_equal "alternate-owned-by", test_object4.human_id
+
+				# Test find by human ID.
+				test_object5 = ::DataObject.view("by_human_id", :key => "alternate-owned-by", :include_docs => true)[0]
+				assert test_object5.valid?
+				assert_equal [], test_object5.path
+				assert_equal "Test Alternate Owned By", test_object5.title
+				assert_equal ["test"], test_object5.tags
+				assert_equal "user/testOwnedBy", test_object5.created_by
+				assert_equal "user/testOwnedBy", test_object5.updated_by
+				assert_equal "user/testOwnedBy", test_object5.owned_by
+				assert_equal "A fake test item to test with.", test_object5.description
+				assert_equal "alternate-owned-by", test_object5.human_id
 			end
 		end
 	end
