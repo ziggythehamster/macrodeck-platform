5cc8fcfb4ee7bf94c7f3ec9dc3107eabd4e6d67d
Keith Gable <ziggy@ignition-project.com>
2010-05-31 21:43:46 -0500

    Test updated by.

--- test/data_object.rb
+++ test/data_object.rb
@@ -35,6 +35,10 @@ module MacroDeck
 				object2["created_by"] = "user/ZiggyTheHamster"
 				object2["title"] = "Test Alternate Created By"
 				object2["human_id"] = "alternate-created-by"
+				object3 = object2.dup
+				object3["title"] = "Test Alternate Updated By"
+				object3["updated_by"] = "user/csakon"
+				object3["human_id"] = "alternate-updated-by"
 
 				test_object = ::DataObject.new(object)
 				assert test_object.valid?
@@ -43,6 +47,10 @@ module MacroDeck
 				test_object2 = ::DataObject.new(object2)
 				assert test_object2.valid?
 				assert test_object2.save
+
+				test_object3 = ::DataObject.new(object3)
+				assert test_object3.valid?
+				assert test_object3.save
 			end
 			
 			# Tests that the previously created record can be retrieved.
@@ -68,6 +76,18 @@ module MacroDeck
 				assert_equal "user/System", test_object2.owned_by
 				assert_equal "A fake test item to test with.", test_object2.description
 				assert_equal "alternate-created-by", test_object2.human_id
+
+
+				test_object3 = ::DataObject.view("by_updated_by", :key => "user/csakon", :include_docs => true)[0]
+				assert test_object3.valid?
+				assert_equal [], test_object3.path
+				assert_equal "Test Alternate Updated By", test_object3.title
+				assert_equal ["test"], test_object3.tags
+				assert_equal "user/ZiggyTheHamster", test_object3.created_by
+				assert_equal "user/csakon", test_object3.updated_by
+				assert_equal "user/System", test_object3.owned_by
+				assert_equal "A fake test item to test with.", test_object3.description
+				assert_equal "alternate-updated-by", test_object3.human_id
 			end
 
 			
