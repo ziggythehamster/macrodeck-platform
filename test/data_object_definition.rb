3f827ca25e324265aeb6cd41135ea991432058a3
Keith Gable <ziggy@ignition-project.com>
2010-06-20 00:49:24 -0500

    Update the DataObjectDefinition test to work with the fact we're inheriting from DataObject

--- test/data_object_definition.rb
+++ test/data_object_definition.rb
@@ -68,12 +68,12 @@ module MacroDeck
 
 			# Tests that the object's properties gets populated.
 			def test_004_defined_object_properties
-				assert_equal "not_required_str",	::MacroDeckTestObject.properties[0].name
-				assert_equal "String",			::MacroDeckTestObject.properties[0].type
-				assert_equal "required_str",		::MacroDeckTestObject.properties[1].name
-				assert_equal "String",			::MacroDeckTestObject.properties[1].type
-				assert_equal "validated_str",		::MacroDeckTestObject.properties[2].name
-				assert_equal "String",			::MacroDeckTestObject.properties[2].type
+				assert_equal "not_required_str",	::MacroDeckTestObject.properties[8].name
+				assert_equal "String",			::MacroDeckTestObject.properties[8].type
+				assert_equal "required_str",		::MacroDeckTestObject.properties[9].name
+				assert_equal "String",			::MacroDeckTestObject.properties[9].type
+				assert_equal "validated_str",		::MacroDeckTestObject.properties[10].name
+				assert_equal "String",			::MacroDeckTestObject.properties[10].type
 			end
 
 			# Tests that the object's validations get populated.
