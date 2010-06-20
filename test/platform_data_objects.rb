4d67c0a5097b12176e00e2699780b5471a45d124
Keith Gable <ziggy@ignition-project.com>
2010-06-20 00:31:56 -0500

    Test the properties.

--- test/platform_data_objects.rb
+++ test/platform_data_objects.rb
@@ -23,13 +23,8 @@ module MacroDeck
 
 			# Tests that country has the appropriate properties.
 			def test_002_defined_object_properties
-				puts ::Country.properties.inspect
-				#assert_equal "not_required_str",	::MacroDeckTestObject.properties[0].name
-				#assert_equal "String",			::MacroDeckTestObject.properties[0].type
-				#assert_equal "required_str",		::MacroDeckTestObject.properties[1].name
-				#assert_equal "String",			::MacroDeckTestObject.properties[1].type
-				#assert_equal "validated_str",		::MacroDeckTestObject.properties[2].name
-				#assert_equal "String",			::MacroDeckTestObject.properties[2].type
+				assert_equal "abbreviation",	::Country.properties[0].name
+				assert_equal "String",		::Country.properties[0].type
 			end
 		end
 	end
