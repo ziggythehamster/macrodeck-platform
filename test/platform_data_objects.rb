27ac9fd4e867918839ffb6e553b7e8d9cceba783
Keith Gable <ziggy@ignition-project.com>
2010-07-05 18:03:03 -0500

    Test Place's properties (though I found that geo, an Array, isn't getting read that way)

--- test/platform_data_objects.rb
+++ test/platform_data_objects.rb
@@ -115,6 +115,33 @@ module MacroDeck
 				assert_equal Validatable::ValidatesPresenceOf,	::Locality.validations[10].class
 				assert_equal :title,				::Locality.validations[10].attribute
 			end
+
+			# Test the behavior of Place.
+			def test_013_place
+				assert_equal ::DataObject,			::Place.superclass
+			end
+
+			# Test the properties of Place.
+			def test_014_place_properties
+				assert_equal "address",				::Place.properties[8].name
+				assert_equal "String",				::Place.properties[8].type
+				assert_equal "postal_code",			::Place.properties[9].name
+				assert_equal "String",				::Place.properties[9].type
+				assert_equal "phone_number",			::Place.properties[10].name
+				assert_equal "String",				::Place.properties[10].type
+				assert_equal "url",				::Place.properties[11].name
+				assert_equal "String",				::Place.properties[11].type
+				assert_equal "geo",				::Place.properties[12].name
+				assert_equal "Array",				::Place.properties[12].type
+				assert_equal "cuisine",				::Place.properties[13].name
+				assert_equal "String",				::Place.properties[13].type
+				assert_equal "features",			::Place.properties[14].name
+				assert_equal "Array",				::Place.properties[14].type
+				assert_equal "parking",				::Place.properties[15].name
+				assert_equal "String",				::Place.properties[15].type
+				assert_equal "hours",				::Place.properties[16].name
+				assert_equal "Hash",				::Place.properties[16].type
+			end
 		end
 	end
 end
