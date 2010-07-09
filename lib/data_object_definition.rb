c6652a243d5cc1060a9f1ebf29eacc06db56d230
Keith Gable <ziggy@ignition-project.com>
2010-07-08 22:34:03 -0500

    This was easy - check for object being the right type or nil. Now every field isn't required ;)

--- lib/data_object_definition.rb
+++ lib/data_object_definition.rb
@@ -42,7 +42,7 @@ module MacroDeck
 							symbol = field[0].to_sym.inspect
 							klass = eval(field[1].split(" ")[0]) # NB: This could potentially be a very unsafe operation...
 							properties << "property #{symbol}, :type => \"#{klass}\"\n"
-							properties << "validates_true_for #{symbol}, :logic => lambda { #{field[0]}.is_a?(::#{klass}) }\n"
+							properties << "validates_true_for #{symbol}, :logic => lambda { (#{field[0]}.is_a?(::#{klass}) || #{field[0]}.nil?) }\n"
 							properties << "validates_presence_of #{symbol}\n" if field[2] == true
 						end
 
