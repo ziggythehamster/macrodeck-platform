50fd0166f6fad581eb85a62dff16049bdc748867
Keith Gable <ziggy@ignition-project.com>
2010-09-28 18:45:59 -0500

    Add couchrest patches (mainly to add search support)

---
+++ lib/macrodeck-platform/couchrest_patches.rb
@@ -0,0 +1,24 @@
+# Add search_by to CouchRest::Design
+class ::CouchRest::Design < Document
+	def search_by(*keys)
+		opts = keys.pop if keys.last.is_a?(Hash)
+		opts ||= {}
+		self['fulltext'] ||= {}
+		method_name = "by_#{keys.join('_and_')}"
+
+		if opts[:index]
+			fulltext = {}
+			fulltext["index"] = opts.delete(:index)
+			if opts[:analyzer]
+				fulltext["analyzer"] = opts.delete(:analyzer)
+			else
+				fulltext["analyzer"] = "standard"
+			end
+			fulltext["defaults"] = opts.delete(:defaults) if opts[:defaults]
+			self["fulltext"][method_name] = fulltext
+		else
+			raise NotImplementedError, "Please specify an index function."
+		end
+		method_name
+	end
+end
