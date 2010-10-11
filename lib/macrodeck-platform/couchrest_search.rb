f7f2d404907bc4a4d030d8c57120f3ca0186d13d
Keith Gable <ziggy@ignition-project.com>
2010-10-10 19:40:24 -0500

    Add search functions to CouchRest objects (particularly ExtendedDocument).

---
+++ lib/macrodeck-platform/couchrest_search.rb
@@ -0,0 +1,19 @@
+# Search functionality additions to CouchRest.
+# <http://intridea.com/2009/9/20/couchdb-lucene-couchdbx-and-couchrest?blog=company>
+
+require "couchrest"
+
+class CouchRest::Database
+	def search(design, index, query, options={})
+		CouchRest.get CouchRest.paramify_url("#{@root}/_fti/#{design}/#{index}", options.merge(:q => query))
+	end
+end
+
+class CouchRest::ExtendedDocument
+	def self.search(index, query, options={})
+		options[:include_docs] = true
+		ret = self.database.search(self.to_s, index, query, options)
+		ret['rows'].collect!{|r| self.new(r['doc'])}
+		ret
+	end
+end
