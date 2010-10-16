00546c1af7a9079a3e4e626d9bbb3c4af1c83109
Keith Gable <ziggy@ignition-project.com>
2010-10-15 23:43:52 -0500

    Don't actually add to the index to see if we have other issues.

--- lib/macrodeck-platform/platform_data_objects.rb
+++ lib/macrodeck-platform/platform_data_objects.rb
@@ -94,7 +94,7 @@ module MacroDeck
 									res.add(doc.description);
 									res.add(new Date(), { \"field\":\"indexed_at\", \"store\":\"yes\" });
 									res.add(doc.path.join('/'), { \"field\":\"path\", \"store\":\"yes\", \"index\":\"not_analyzed\" });
-									res.add(new Date(dtstart), { \"field\":\"start_time\", \"type\":\"date\", \"store\":\"yes\" });
+									//res.add(dtstart, { \"field\":\"start_time\", \"type\":\"date\", \"store\":\"yes\" });
 									return res;
 								}
 							  }"
