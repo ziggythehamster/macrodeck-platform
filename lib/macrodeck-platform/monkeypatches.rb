cfe5fa389b8692e53cfbd4605ca632f5a905491c
Keith Gable <ziggy@ignition-project.com>
2010-10-26 22:25:54 -0500

    CouchDB is also particular about the order of startkey and endkey, so those have been added explicitly in the correct order.

--- lib/macrodeck-platform/monkeypatches.rb
+++ lib/macrodeck-platform/monkeypatches.rb
@@ -9,10 +9,12 @@ module CouchRest
 				prequery << "reduce=#{CGI.escape(params.delete(:reduce).to_s)}" if params.include? :reduce
 				prequery << "group=#{CGI.escape(params.delete(:group).to_s)}" if params.include? :group
 				prequery << "group_level=#{CGI.escape(params.delete(:group_level).to_s)}" if params.include? :group_level
+				prequery << "startkey=#{CGI.escape(params.delete(:startkey).to_json.to_s)}" if params.include? :startkey
+				prequery << "endkey=#{CGI.escape(params.delete(:endkey).to_json.to_s)}" if params.include? :endkey
 				prequery = prequery.join("&")
 				prequery << "&" if prequery.length > 0 # needed so that query and prequery go together.
 				query = params.collect do |k,v|
-					v = v.to_json if %w{key startkey endkey}.include?(k.to_s)
+					v = v.to_json if %w{key}.include?(k.to_s)
 					"#{k}=#{CGI.escape(v.to_s)}"
 				end.join("&")
 				url = "#{url}?#{prequery}#{query}"
