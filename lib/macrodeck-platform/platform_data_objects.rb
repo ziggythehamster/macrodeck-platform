92fdddde5b901c9700bc9237cdeb13bf3fdafeb8
Keith Gable <ziggy@ignition-project.com>
2010-11-16 22:00:07 -0600

    Attempt to use nil instead of Hash to fix the cast error

--- lib/macrodeck-platform/platform_data_objects.rb
+++ lib/macrodeck-platform/platform_data_objects.rb
@@ -260,7 +260,7 @@ module MacroDeck
 						["postal_code", "String", false],
 						["phone_number", "String", false],
 						["url", "String", false],
-						["geo", "Hash", false], # { 'type': 'Point', 'coordinates': [lat, lng] } - GeoJSON
+						["geo", nil, false], # { 'type': 'Point', 'coordinates': [lat, lng] } - GeoJSON
 						["fare", ["String"], false],
 						["features", ["String"], false],
 						["parking", "String", false],
