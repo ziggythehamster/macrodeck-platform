2032bf08c75bd04452832a29120337ac1185a597
Keith Gable <ziggy@ignition-project.com>
2010-11-24 23:41:30 -0600

    Add spatial search functions. They probably don't work though.

--- lib/macrodeck-platform/couchrest_search.rb
+++ lib/macrodeck-platform/couchrest_search.rb
@@ -7,6 +7,11 @@ class CouchRest::Database
 	def search(design, index, query, options={})
 		CouchRest.get CouchRest.paramify_url("#{@root}/_fti/_design/#{design}/#{index}", options.merge(:q => query))
 	end
+
+	# Perform a spatial search. +bbox+ should be specified as an array.
+	def spatial_search(design, index, bbox, options={})
+		CouchRest.get CouchRest.paramify_url("#{@root}/_design/#{design}/_spatial/#{index}", options.merge(:bbox => bbox.join(","))
+	end
 end
 
 class CouchRest::ExtendedDocument
@@ -14,6 +19,13 @@ class CouchRest::ExtendedDocument
 		options[:include_docs] = true
 		ret = self.database.search(self.to_s, index, query, options)
 		ret['rows'].collect!{|r| self.new(r['doc'])}
+		ret
+	end
+
+	# Perform a spatial search. +bbox+ should be specified as an array.
+	def self.spatial_search(index, bbox, options={})
+		ret = self.database.spatial_search(self.to_s, index, bbox, options)
+		ret['rows'].collect! { |r| self.get(r['_id']) }
 		ret
 	end
 end
