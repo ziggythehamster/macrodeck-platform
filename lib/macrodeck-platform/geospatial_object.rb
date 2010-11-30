78f101fc5b0e9d1c44f36899dd591843fd1eff4d
Keith Gable <ziggy@ignition-project.com>
2010-11-29 21:13:07 -0600

    Add a class for processing geospatial objects... Currently has a function to calculate distance.

---
+++ lib/macrodeck-platform/geospatial_object.rb
@@ -0,0 +1,36 @@
+# This class is a simple "wrapper" class to perform geospatial math and distance calculation.
+class GeospatialObject
+	attr_reader :data_object
+
+	# GeospatialObject must be created with a passed data_object.
+	def initialize(data_object)
+		@data_object = data_object
+	end
+
+	# Returns the latitude of the data object.
+	def lat
+		@data_object.lat
+	end
+
+	# Returns the longitude of the data object.
+	def lng
+		@data_object.lng
+	end
+
+	# Return the distance to the passed object.
+	#
+	# Pass the earth's radius as well if you need it in km instead
+	# of miles.
+	#
+	# Ported from: <http://www.movable-type.co.uk/scripts/latlong.html>
+	def distance_to(geospatial_object, earth_radius = 3963.1676)
+		diff_lat = (geospatial_object.lat - self.lat).to_radians
+		diff_lng = (geospatial_object.lng - self.lng).to_radians
+		a = Math.sin(diff_lat/2)**2 +
+			Math.cos(self.lat.to_radians) * Math.cos(geospatial_object.lat.to_radians) *
+			Math.sin(diff_lng/2)**2
+		c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
+		d = earth_radius * c
+		return d
+	end
+end
