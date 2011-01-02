1587371b93610ad7fc97ea1df6b5fe004203c9e1
Keith Gable <ziggy@ignition-project.com>
2011-01-01 22:19:12 -0600

    Add a view to check for missing foursquare venue IDs

--- lib/macrodeck-platform/platform_data_objects.rb
+++ lib/macrodeck-platform/platform_data_objects.rb
@@ -582,6 +582,16 @@ module MacroDeck
 							}
 						  }",
 						  "reduce" => "_count"
+						},
+						# Return places that have a blank foursquare_venue_id (and not blank geo)
+						{ "view_by" => "missing_foursquare_venue_id",
+						  "map" =>
+						  "function(doc) {
+							if (doc['couchrest-type'] == 'Place' && !doc['foursquare_venue_id'] && doc['geo']) {
+								emit(doc['_id'], 1);
+							}
+						  }",
+						  "reduce" => "_count"
 						}
 					]
 				}.freeze
