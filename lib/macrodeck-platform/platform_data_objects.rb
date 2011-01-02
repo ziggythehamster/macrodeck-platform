e9d94c3afd5715e11af72098941801ff443cf682
Keith Gable <ziggy@ignition-project.com>
2011-01-02 00:16:17 -0600

    Make tips be an array

--- lib/macrodeck-platform/platform_data_objects.rb
+++ lib/macrodeck-platform/platform_data_objects.rb
@@ -328,7 +328,7 @@ module MacroDeck
 						["reservations", "String", false],
 						["bitly_hash", "String", false],
 						["foursquare_venue_id", "String", false],
-						["tips", "Array", false]
+						["tips", ["String"], false]
 					],
 					"fulltext" => [
 						["common_fields",
