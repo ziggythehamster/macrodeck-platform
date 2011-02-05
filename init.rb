67d16279b25f8de698aa5b1a6eccecc9da8af61a
Keith Gable <ziggy@ignition-project.com>
2011-02-04 22:58:07 -0600

    Use unshift to make the platform load first

--- init.rb
+++ init.rb
@@ -1,5 +1,5 @@
 # Load required files and libraries
-$LOAD_PATH << File.join(File.dirname(__FILE__), "lib")
+$LOAD_PATH.unshift File.join(File.dirname(__FILE__), "lib")
 
 require "rubygems"
 require "validatable"
