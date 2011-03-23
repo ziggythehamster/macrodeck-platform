102fc95f94188dad35ac2c5e398a557c55703178
Keith Gable <ziggy@ignition-project.com>
2011-03-22 23:06:51 -0500

    Fix an error with said debugging

--- lib/macrodeck-platform/couchrest_search.rb
+++ lib/macrodeck-platform/couchrest_search.rb
@@ -59,7 +59,7 @@ class CouchRest::ExtendedDocument
 				gs_obj = GeospatialObject.new([res["bbox"][0], res["bbox"][1]])
 				res_ids_with_dist << [ gs_obj.distance_to(center), res["id"] ]
 			end
-			puts res_ids_With_dist.inspect
+			puts res_ids_with_dist.inspect
 			if res_ids_with_dist.length > 0
 				res_ids_with_dist.sort_by { |r| r[0] }
 				res_ids = res_ids_with_dist.collect { |r| r[1] }
