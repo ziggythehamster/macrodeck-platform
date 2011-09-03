8fc52383fb911db6a48ea6dfe9fad6897d3bc4a9
Keith Gable <ziggy@ignition-project.com>
2011-09-02 21:57:53 -0500

    Actually make this class do something now.

--- lib/macrodeck-platform/turk_task.rb
+++ lib/macrodeck-platform/turk_task.rb
@@ -1,15 +1,20 @@
 class TurkTask
-	attr_reader :item
-	attr_reader :task
+	attr_reader :id
+	attr_reader :prerequisites
+	attr_reader :title
+	attr_reader :answer_count
 	attr_reader :field
 
-	def initialize(item, task)
-		@item = item
-		@task = task
-		@item.turk_fields.each do |tf|
-			if tf["name"] == @task.turk_field
+	def initialize(obj, task)
+		# Link the field in both records.
+		obj.turk_fields.each do |tf|
+			if tf["name"] == task["turk_field"]
 				@field = tf
 			end
 		end
+		@id = task["id"]
+		@prerequisites = task["prerequisites"]
+		@title = task["title"]
+		@answer_count = task["answer_count"]
 	end
 end
