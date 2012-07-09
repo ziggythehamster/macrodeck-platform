c7a28316fb964fd4c5c890b7cfa7ea83d88f60bc
Keith Gable <ziggy@ignition-project.com>
2012-07-08 21:27:10 -0500

    Specify the behaviors to use for each task.

--- lib/macrodeck-platform/platform_data_objects.rb
+++ lib/macrodeck-platform/platform_data_objects.rb
@@ -19,9 +19,9 @@ module MacroDeck
 					"has_attachment" => true,
 					"turk_fields" => [
 						{ "name" => "DaysOfWeek", "type" => ["Integer#DayOfWeek"], "title" => "Days of week" },
-						{ "name" => "StartTime", "type" => "Time", "title" => "Start time", "params" => { "hide_date" => true } },
-						{ "name" => "EndTime", "type" => "Time", "title" => "End time", "params" => { "hide_date" => true } },
-						{ "name" => "Title", "type" => "String", "object_type" => "Event", "object_field" => "title" }
+						{ "name" => "StartTime", "type" => "Time#StartTime", "title" => "Start time", "params" => { "hide_date" => true } },
+						{ "name" => "EndTime", "type" => "Time#EndTime", "title" => "End time", "params" => { "hide_date" => true } },
+						{ "name" => "Title", "type" => "String#Title", "object_type" => "Event", "object_field" => "title" }
 					],
 					"views" => [
 						{ "view_by" => "turk_unanswered",
